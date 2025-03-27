4b0b2b836c0be3b11f8d2c3bc7aafd420d3af268
wabwirevalentine <valentine.wabwire@tracom.co.ke>
2025-03-27 15:56:35 +0300

    object operations

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution in this file!
+const employee = {
+  name: "Valentine",
+  streetAddress: "",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+// Example usage:
+const updatedEmployee = updateEmployeeWithKeyAndValue(
+  employee,
+  "streetAddress",
+  "Mombasa"
+);
+console.log(updatedEmployee);
+console.log(employee);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+const updatedEmployeed = destructivelyUpdateEmployeeWithKeyAndValue(
+  employee,
+  "streetAddress",
+  "Mombasa"
+);
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+
+  delete newEmployee[key];
+
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+
+  return employee;
+}
